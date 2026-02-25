4220c31ad30f8ec13861e19f3f6d880a8ce05eb8
bharatpatel9123-dot <bharatpatel9123@gmail.com>
2026-02-25 16:58:16 -0500

    Update Assignment1Program1.java

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,14 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    System.out.println("Block Letters 'BP'");
+    System.out.println();
+    System.out.println("BBBBBB      PPPPPP   ");
+    System.out.println("BBB   BBB   PPP   PPP");
+    System.out.println("BBB   BBB   PPP   PPP");
+    System.out.println("BBBBBB      PP PPP   ");
+    System.out.println("BBB   BBB   PPP      ");
+    System.out.println("BBB   BBB   PPP      ");
+    System.out.println("BBBBBB      PPP      ");
 
   }
 }
